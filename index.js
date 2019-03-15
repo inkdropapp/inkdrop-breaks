65d2e895485356f06b2e5e8f892ab0cd46496c5f
Takuya Matsuyama <hi@craftz.dog>
2019-03-15 14:06:00 +0900

    feat(*): Support Inkdrop 4.x

--- index.js
+++ index.js
@@ -1,19 +1,18 @@
-var breaks = require('remark-breaks')
+const { markdownRenderer } = require('inkdrop')
+const breaks = require('remark-breaks')
 
 module.exports = {
-  activate () {
-    const { MDEPreview } = inkdrop.components.classes
-    if (MDEPreview) {
-      return MDEPreview.remarkPlugins.splice(0, 0, breaks)
-    }
+  activate() {
+    return markdownRenderer.remarkPlugins.splice(0, 0, breaks)
   },
 
-  deactivate () {
-    const { MDEPreview } = inkdrop.components.classes
-    if (MDEPreview) {
-      MDEPreview.remarkPlugins = MDEPreview.remarkPlugins.filter(function (plugin) {
-        return plugin !== breaks
-      })
+  deactivate() {
+    if (markdownRenderer) {
+      markdownRenderer.remarkPlugins = markdownRenderer.remarkPlugins.filter(
+        plugin => {
+          return plugin !== breaks
+        }
+      )
     }
   }
 }
